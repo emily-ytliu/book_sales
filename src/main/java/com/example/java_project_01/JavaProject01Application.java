a5a1e6606e1d4b4b88a86b4d93be21673a0235b6
yiting <yiting50425@gmail.com>
2023-04-24 13:44:45 +0800

    first commit

---
+++ src/main/java/com/example/java_project_01/JavaProject01Application.java
@@ -0,0 +1,13 @@
+package com.example.java_project_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProject01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_project_01/JavaProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_project_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
